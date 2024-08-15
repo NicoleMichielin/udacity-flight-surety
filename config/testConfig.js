be7d3dcb97fd9943dc98ac677918731940bdebad
NicoleMichielin <michielinnicole@gmail.com>
2024-08-15 13:04:53 +0200

    Update testConfig.js

--- config/testConfig.js
+++ config/testConfig.js
@@ -7,29 +7,26 @@ var Config = async function(accounts) {
     
     // These test addresses are useful when you need to add
     // multiple users in test scripts
-    /*let testAddresses = [
-        "0x69e1CB5cFcA8A311586e3406ed0301C06fb839a2",
-        "0xF014343BDFFbED8660A9d8721deC985126f189F3",
-        "0x0E79EDbD6A727CfeE09A2b1d0A59F7752d5bf7C9",
-        "0x9bC1169Ca09555bf2721A5C9eC6D69c8073bfeB4",
-        "0xa23eAEf02F9E0338EEcDa8Fdd0A73aDD781b2A86",
-        "0x6b85cc8f612d5457d49775439335f83e12b8cfde",
-        "0xcbd22ff1ded1423fbc24a7af2148745878800024",
-        "0xc257274276a4e539741ca11b590b9447b26a8051",
-        "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
-    ];*/
-
-    let testAddresses = accounts;
-    let owner = accounts[0];
-    let firstAirline = accounts[1];
+    let testAddresses = [
+        "0x0fc66b02fbc4851f3cfbaeba23656e930413a773",
+        "0x97fb2cc1650836c26fce9f65c1fa0595a2f3e517",
+        "0x1e5bd1d13e33a380a20a959edd58c6967cd400bd",
+        "0x19de3c4c642d2ea56ed3f4f0d83316a3a51ddc52",
+        "0x99ddbf7d82773d5f8efc968afb8fd507e501637e",
+        "0x9f09c5442038b7795a857e71c0863fb0c9d3a31c",
+        "0x87b8a3438eb9c60c0313a6a3358f06a12606ab69",
+        "0x52edce91676dfcd66e856b1ea195d046a98ea1b3",
+        "0xb9416bebec10b230e21b1323968ce90188d2b374",
+        "0x07836f0264e97a36b6f0a2ff2d7add32f876ea10",
+    ];
 
     let flightSuretyData = await FlightSuretyData.new();
     let flightSuretyApp = await FlightSuretyApp.new();
 
     
     return {
-        owner: owner,
-        firstAirline: firstAirline,
+        owner: accounts[0],
+        firstAirline: accounts[1],
         weiMultiple: (new BigNumber(10)).pow(18),
         testAddresses: testAddresses,
         flightSuretyData: flightSuretyData,
